0b37bc04d95e123dac352f1720c25462101b0603
wilberchicacarvajal <wilberpros@gmail.com>
2023-04-20 19:40:08 -0500

    se agregan los botones a la tabla y se crean los diferentes iconos,atributos y clases. Ademas se usa el data para agregar información a los botones sobre el teacher

--- src/assets/js/teachers/operations.js
+++ src/assets/js/teachers/operations.js
@@ -23,26 +23,57 @@ function listTeachers() { // esto es para listar a los profesores
     tbody.innerHTML = '';
 
     arrayTeachers.forEach( (teacher, index) => {  //foreach me retorna o recorre cada posición del objeto y su posición
+
+        const {name, description, email, birthDate} = teacher; // esto se llama desestructuración permite desempacar valores o arreglos o propiedades de objetos de distintas variables
+        
+
         // creo la fila 
         const row = document.createElement('tr');
+        row.classList.add('align-middle');
 
-        // creo las columnas
+        // creo las columnas 
         const colId = document.createElement('td');
         colId.textContent = index;
+        colId.classList.add('text-center');
 
         const colName = document.createElement('td');
-        colName.textContent = teacher.name;
+        colName.textContent = name;
 
         const colDescription = document.createElement('td');
-        colDescription.textContent= teacher.description;
+        colDescription.textContent= description;
 
         const colEmail = document.createElement('td');
-        colEmail.textContent = teacher.email;
+        colEmail.textContent = email;
 
         const colBirthDate = document.createElement('td');
-        colBirthDate.textContent = teacher.birthDate;
+        colBirthDate.textContent = birthDate;
 
         const colButtons = document.createElement('td');
+        colButtons.classList.add('text-center');
+
+        const editButton = document.createElement('button');
+        editButton.classList.add('btn', 'btn-primary', 'btn-edit', 'm-1');
+        editButton.dataset.id = index;
+        editButton.setAttribute('title', 'Editar');
+        editButton.setAttribute('type', 'button')
+
+        const editIcon = document.createElement('em');
+        editIcon.classList.add('fa', 'fa-pencil');
+        editButton.appendChild(editIcon);
+
+        colButtons.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('btn', 'btn-danger', 'btn-delete', 'm-1');
+        deleteButton.dataset.id = index;
+        deleteButton.setAttribute('title', 'Eliminar');
+        deleteButton.setAttribute('type', 'button');
+
+        const deleteIcon = document.createElement('em');
+        deleteIcon.classList.add('fa', 'fa-trash',);
+        deleteButton.appendChild(deleteIcon);
+
+        colButtons.appendChild(deleteButton);
         
 
         //agrego las columnas a la fila
